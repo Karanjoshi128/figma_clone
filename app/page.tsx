4c4461a5d88f83a1ff0db75864cd42c24b3abe57
Karanjoshi128 <joshikaran.aad.0007@gmail.com>
2025-05-01 19:34:32 +0530

    feat: update dependencies and configure Tailwind CSS
    - Changed the development script to use Turbopack for improved performance.
    - Updated React and React DOM to version 18.3.1.
    - Added @liveblocks/react-comments dependency.
    - Updated Tailwind CSS and added tailwindcss-animate for animations.
    - Created postcss.config.js to include Tailwind CSS and Autoprefixer.
    - Added tailwind.config.ts with custom theme and animation configurations.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+      <h1 className="text-9xl">Hello duniya</h1>
     </Room>
   );
 }
